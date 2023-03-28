6dd6ce5d0fb2538f3a24b62d244ee4e8f8ed149d
Dharshan <66472781+im-kronos@users.noreply.github.com>
2023-03-28 12:06:22 +0530

    Create encrypdec.java

---
+++ encrypdec.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package encrypdecryp;
+
+
+/**
+ *
+ * @author 91861
+ */
+public class EncrypDecryp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        StartPage s = new StartPage();
+        s.setVisible(true);
+    }
+    
+}
+
+//need to add database
+// need to add images with the background size with high quality
+// need to to add some file saving options
+// file saving idea create save btn and popup a new window ask the purpose of the name and the encrypted text and
+// click on save and ask the loactaion of the file to save!
+// it should like save btn should be diabled untill the generate is clicked
