61f7f08b3a7c4abf2d43c9d649c12f93583a5257
Dharshan <66472781+im-kronos@users.noreply.github.com>
2023-04-07 00:08:06 +0530

    Update aes.java

--- aes.java
+++ aes.java
@@ -1,30 +1,46 @@
-  
-import javax.swing.JOptionPane; 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
 import java.util.Base64;
-import java.io.*;
-import java.util.*;
-public class Encryption {
+import java.util.Scanner;
+import javax.swing.*;
+
+public class aesUI {
     private SecretKey key;
     private final int KEY_SIZE = 128;
     private final int DATA_LENGTH = 128;
     private Cipher encryptionCipher;
+
+    public aesUI() {
+        try {
+            init();
+            Scanner scanner = new Scanner(System.in);
+            String data = JOptionPane.showInputDialog( "Enter text to be Encrypted and Decrypted using AES" ); 
+           
+            String encryptedData = encrypt(data);
+            String decryptedData = decrypt(encryptedData);
+            JOptionPane.showMessageDialog(null,"The encrtpted text is " + encryptedData);  
+            JOptionPane.showMessageDialog(null,"The decrtpted text is " + decryptedData);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
 
     public void init() throws Exception {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
         keyGenerator.init(KEY_SIZE);
         key = keyGenerator.generateKey();
     }
-public String encrypt(String data) throws Exception {
+
+    public String encrypt(String data) throws Exception {
         byte[] dataInBytes = data.getBytes();
         encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
         encryptionCipher.init(Cipher.ENCRYPT_MODE, key);
         byte[] encryptedBytes = encryptionCipher.doFinal(dataInBytes);
         return encode(encryptedBytes);
     }
+
     public String decrypt(String encryptedData) throws Exception {
         byte[] dataInBytes = decode(encryptedData);
         Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
@@ -33,33 +49,16 @@ public String encrypt(String data) throws Exception {
         byte[] decryptedBytes = decryptionCipher.doFinal(dataInBytes);
         return new String(decryptedBytes);
     }
-private String encode(byte[] data) {
+
+    private String encode(byte[] data) {
         return Base64.getEncoder().encodeToString(data);
     }
 
     private byte[] decode(String data) {
         return Base64.getDecoder().decode(data);
     }
-public static void main(String[] args) {
-        try {
-            Encryption aes_encryption = new Encryption();
-            String data = JOptionPane.showInputDialog( "Enter text to be Encrypted and Decrypted using AES" ); 
-            aes_encryption.init();
-            Scanner sc=new Scanner(System.in);
-           // System.out.println("Enter text to be encrypted");
-           // String data=sc.nextLine();
-            String encryptedData = aes_encryption.encrypt(data);
-            String decryptedData = aes_encryption.decrypt(encryptedData);
-            // JOptionPane.showMessageDialog(null,"The encrtpted text is " + encryptedData, JOptionPane.PLAIN_MESSAGE ); 
 
-           
-
-            
-            JOptionPane.showMessageDialog(null,"The encrtpted text is " + encryptedData);  
-            JOptionPane.showMessageDialog(null,"The decrtpted text is " + decryptedData);
-            //System.out.println("Encrypted Data : " + encryptedData);
-            //System.out.println("Decrypted Data : " + decryptedData);
-        } catch (Exception ignored) {
-        }
+    public static void main(String[] args) {
+        aesUI aesEncryption = new aesUI();
     }
 }
